5324f85881466140567acdc5761539d46f5fccc1
nawid <nnd1380@gmail.com>
2022-09-12 12:14:19 +0430

    fix mobile

--- app/client/src/components/mobile-component/result/StrategyResult.jsx
+++ app/client/src/components/mobile-component/result/StrategyResult.jsx
@@ -1,15 +1,25 @@
-//in this component we show the result of the strategy 
-import React from "react";
+//in this component we show the result of the strategy
+import React from 'react';
 //style
-import './StrategyResult.css';
+import './strategyResult.css';
 //icon
 import next from '../../../assets/icon/next.svg';
 import star from '../../../assets/icon/star.svg';
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 //link
 import applicant from '../../../Applicant.json';
 
-export function StrategyResult({ bigImg, smallImg, name, author, returnRate, strategyImg, winRate, riskReward, timeFrame }) {
+export function StrategyResult({
+  bigImg,
+  smallImg,
+  name,
+  author,
+  returnRate,
+  strategyImg,
+  winRate,
+  riskReward,
+  timeFrame,
+}) {
   return (
     <div className="mb-strategy-result-content">
       <div className="mb-strategy-result-first-row">
@@ -25,12 +35,16 @@ export function StrategyResult({ bigImg, smallImg, name, author, returnRate, str
         <img className="mb-strategy-img-result" src={strategyImg} />
       </div>
       <div className="mb-strategy-result-second-row">
-        <pre>Win Rate:     <span style={{ color: 'green' }}>{winRate}%</span></pre>
-        <pre>Risk/Reward:  <span style={{ color: 'green' }}>{riskReward}</span>   </pre>
+        <pre>
+          Win Rate: <span style={{ color: 'green' }}>{winRate}%</span>
+        </pre>
+        <pre>
+          Risk/Reward: <span style={{ color: 'green' }}>{riskReward}</span>{' '}
+        </pre>
       </div>
       <div className="mb-strategy-result-third-row">
         <pre>Time Frame:{timeFrame}%</pre>
-        <pre>  Return:           {returnRate}h</pre>
+        <pre> Return: {returnRate}h</pre>
       </div>
       <Link to={applicant.mobileBuyStrategy}>
         <img className=" mb-strategy-result-next-icon" src={next} />
