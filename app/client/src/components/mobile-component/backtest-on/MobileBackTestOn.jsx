abad81ba993f180d9df238b874f2503dc30d03c4
nawid <nnd1380@gmail.com>
2022-09-13 21:36:31 +0430

    fix analyse page

--- app/client/src/components/mobile-component/backtest-on/MobileBackTestOn.jsx
+++ app/client/src/components/mobile-component/backtest-on/MobileBackTestOn.jsx
@@ -1,13 +1,19 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 //icon
 import more from '../../../assets/icon/more.svg';
 import less from '../../../assets/icon/less.svg';
 //component
-import { MobileButton } from "../button/MobileButton";
+import { MobileButton } from '../button/MobileButton';
 //styles
-import './mobileBackTestOn.css';
+import './mobileBacktestOn.css';
 
-export function MobileBackTestOn({ handleClick, moreBackTest, bigImg, smallImg, hasIcon }) {
+export function MobileBackTestOn({
+  handleClick,
+  moreBackTest,
+  bigImg,
+  smallImg,
+  hasIcon,
+}) {
   if (hasIcon) {
     return (
       <div className="mb-buyStrategy-backtest">
@@ -17,21 +23,30 @@ export function MobileBackTestOn({ handleClick, moreBackTest, bigImg, smallImg, 
               <img className="mb-buyStrategy-img-big" src={bigImg} />
               <img className="mb-buyStrategy-img-small" src={smallImg} />
             </div>
-            <span className="big">USDJPY </span><span className="small"> in 1 hour time frame</span>
+            <span className="big">USDJPY </span>
+            <span className="small"> in 1 hour time frame</span>
           </div>
-          {!moreBackTest ? <img className='mb-more-icon-buyStrategy'
-            aria-label="more"
-            id='moreBacktest'
-            onClick={handleClick}
-            src={more} /> : <img className='mb-more-icon-buyStrategy'
+          {!moreBackTest ? (
+            <img
+              className="mb-more-icon-buyStrategy"
+              aria-label="more"
+              id="moreBacktest"
+              onClick={handleClick}
+              src={more}
+            />
+          ) : (
+            <img
+              className="mb-more-icon-buyStrategy"
               aria-label="less"
               id="lessBacktest"
               onClick={handleClick}
-              src={less} />}
+              src={less}
+            />
+          )}
         </div>
       </div>
     );
-  }else{
+  } else {
     return (
       <div className="mb-buyStrategy-backtest">
         <div className="mb-buyStrategy-first-row">
@@ -40,7 +55,8 @@ export function MobileBackTestOn({ handleClick, moreBackTest, bigImg, smallImg, 
               <img className="mb-buyStrategy-img-big" src={bigImg} />
               <img className="mb-buyStrategy-img-small" src={smallImg} />
             </div>
-            <span className="big">USDJPY </span><span className="small"> in 1 hour time frame</span>
+            <span className="big">USDJPY </span>
+            <span className="small"> in 1 hour time frame</span>
           </div>
         </div>
       </div>
